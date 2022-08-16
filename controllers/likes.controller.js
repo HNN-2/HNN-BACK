cf1c018b38fef8a13901fcca99f35e3a7397cd49
Minsun91 <minsunkim167@gmail.com>
2022-08-16 09:56:30 +0200

    좋아요 등록/취소 가능

--- controllers/likes.controller.js
+++ controllers/likes.controller.js
@@ -1,20 +1,21 @@
 const LikesService = require("../services/likes.service");
+
 class LikesController {
     likeService = new LikesService();
 
+    findLikeId = async (req, res, next) => {
+        const { userId } = res.locals;
+        const findLikeId = await this.likeService.findLikeId(userId);
+        res.json({ success: true, data: findLikeId });
+    };
+
+    //게시글 좋아요 확인 후 등록/취소
     postLike = async (req, res, next) => {
         const { postId } = req.params;
         const { userId } = res.locals;
 
         const postLikeData = await this.likeService.postLike(postId, userId);
-        res.json(postLikeData.msg);
-    };
-
-    getMyLike = async (req, res) => {
-        const { userId } = req.locals;
-        const getMyLike = await this.likeService.getMyLike(userId);
-
-        res.json(getMyLike);
+        res.json({ success: true, msg: postLikeData.msg });
     };
 }
 
--- routes/like.routes.js
+++ routes/like.routes.js
@@ -5,9 +5,9 @@ const LikesController = require("../controllers/likes.controller");
 const likesController = new LikesController();
 
 // 게시물 좋아요
-router.patch("/like/:postId", authMiddleware, likesController.postLike);
+router.patch("/:postId", authMiddleware, likesController.postLike);
 
 // 자기가 좋아요 한 게시물 조회
-router.get("/my", authMiddleware, likesController.getMyLike);
+// router.get("/my", authMiddleware, likesController.getMyLike);
 
 module.exports = router;
