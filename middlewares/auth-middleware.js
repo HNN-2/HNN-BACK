9d5594e82eef19c956a1c4e5d4828cd55562c04f
gwonyeong <qwe980121@naver.com>
2022-08-16 17:27:33 +0900

    토큰은 authorization 에 넣도록 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -6,9 +6,10 @@ const signRepository = new SignRepository();
 
 module.exports = async(req, res, next) => {
     try {
-        const token = req.cookies.token; //토큰 정상 출력
+        const { authorization } = req.headers;
+        const [Type, token] = (authorization || "").split(" "); //토큰 정상 출력
 
-        if (!token) {
+         if (!token || Type !== "Bearer") {
             res.send({
                 errorMessage: "로그인 후 사용하세요",
             });
