34446f0e037e976bd3a4c472300ec612e5dad656
Minsun91 <minsunkim167@gmail.com>
2022-08-17 21:02:05 +0200

    에러처리 다시 봐야함

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,33 +1,49 @@
 const { Like, Post, User, Comment } = require("../models");
 const PostService = require("../services/posts.service");
 const SignRepository = require("../repositories/sign.repository");
+const { connect } = require("mongoose");
 
 class PostRepository {
     //게시글 전체 보기
-    //17일 오전에 댓글 숫자 나오게 수정 완료
-    findAllPost = async () => {
+    //17일 밤 유저가 좋아요한 포스트 true/false로 구현
+    findAllPost = async (userId) => {
         const posts = await Post.findAll();
         const Locals = [];
         const like = [];
         const CommentNum = [];
+        const arr = [];
 
         for (let i = 0; i < posts.length; i++) {
             const locals = await User.findOne({
                 where: { userId: posts[i].userId },
-            }); //locals에는 posts.userId로 찾은 user 데이터가 담겨있다.
+            }); //해당 게시물의 유저 정보 출력
 
             const temp = await Like.findAll({
                 where: { postId: posts[i].postId },
-            });
+            }); //해당 게시물의 좋아요 개수 출력
+
             const commentNum = await Comment.findAll({
                 where: { postId: posts[i].postId },
+            }); //해당 게시물의 댓글 개수 출력
+
+            //user가 좋아요한 게시글 보여주기
+            const FindUserLikeId = await Like.findOne({
+                where: { userId: 2, postId: posts[i].postId },
+                raw: true,
             });
+
+            if (FindUserLikeId) {
+                arr.push(true);
+            } else {
+                arr.push(false);
+            }
 
             like.push(temp.length);
             Locals.push(locals);
             CommentNum.push(commentNum.length);
         }
-        return { posts, Locals, like, CommentNum };
+
+        return { posts, Locals, like, CommentNum, arr };
     };
 
     //댓글 없는 게시글 상세 조회
--- services/posts.service.js
+++ services/posts.service.js
@@ -4,14 +4,14 @@ class PostService {
     postRepository = new PostRepository();
 
     //게시글 전체 보기
-    findAllPost = async () => {
+    findAllPost = async (userId) => {
         const allPost = await this.postRepository.findAllPost();
+
         const Posts = allPost.posts.map((post, i) => {
             const Locals = allPost.Locals;
             const like = allPost.like;
+            const arr = allPost.arr;
 
-            //위에 map(post,i)가 있어서 굳이 for 문 돌리지 않아도 된다고 함.. 그리고 됨!
-            // for (let i = 0; i <Locals.length; i++) {
             return {
                 postId: post.postId,
                 title: post.title,
@@ -21,13 +21,13 @@ class PostService {
                 MBTI: allPost.Locals[i].dataValues.MBTI,
                 createdAt: post.createdAt,
                 like: allPost.like[i],
+                likepost: allPost.arr[i],
                 commentNum: allPost.CommentNum[i],
                 info: {
                     songTitle: post.songTitle,
                     singer: post.singer,
                 },
             };
-            // }
         });
 
         //게시글 내림 차순으로 정렬
@@ -42,18 +42,25 @@ class PostService {
 
     //게시글 상세조회
     getPost = async (postId) => {
-        const getPostData = await this.postRepository.findOnePost(postId);
-        const getCommentData = await this.postRepository.findComments(postId);
-        const postLikeData = getPostData.dataValues.Likes;
-        const commentUserDate = getPostData.dataValues.Users;
+        if (!postId) {
+            return { success: false };
+        } else {
+            const getPostData = await this.postRepository.findOnePost(postId);
+            const getCommentData = await this.postRepository.findComments(
+                postId
+            );
+            const postLikeData = getPostData.dataValues.Likes;
+            const commentUserDate = getPostData.dataValues.Users;
 
-        delete getPostData.dataValues.Likes; //배열 지우는 방법
+            delete getPostData.dataValues.Likes; //배열 지우는 방법
 
-        return {
-            poster: getPostData,
-            like: postLikeData.length,
-            commenter: getCommentData,
-        };
+            return {
+                success: true,
+                poster: getPostData,
+                like: postLikeData.length,
+                commenter: getCommentData,
+            };
+        }
     };
     //게시글 생성
     createPost = async (
@@ -65,6 +72,11 @@ class PostService {
         userId,
         MBTI
     ) => {
+        //입력 유무 확인
+        if (title || content || imageUrl || songTitle || singer == undefined) {
+            return { success: false };
+        }
+
         await this.postRepository.createPost(
             title,
             content,
@@ -82,18 +94,48 @@ class PostService {
     };
 
     //게시글 수정
-    updatePost = async (postId, title, content, imageUrl) => {
-        await this.postRepository.updatePost(postId, title, content, imageUrl);
-        return {
-            success: true,
-            msg: "게시물이 수정되었습니다.",
-        };
+    updatePost = async (
+        postId,
+        title,
+        content,
+        imageUrl,
+        songTitle,
+        singer
+    ) => {
+        if (
+            !postId ||
+            title ||
+            content ||
+            imageUrl ||
+            songTitle ||
+            singer == undefined
+        ) {
+            return {
+                success: false,
+                msg,
+            };
+        } else {
+            await this.postRepository.updatePost(
+                postId,
+                title,
+                content,
+                imageUrl
+            );
+            return {
+                success: true,
+                msg,
+                // msg: "게시물이 수정되었습니다.",
+            };
+        }
     };
 
     //게시글 삭제
     deletePost = async (postId) => {
+        if (!postId) {
+            return { success: false, msg };
+        }
         await this.postRepository.deletePost(postId);
-        return { success: true, msg: "게시물 삭제에 성공했습니다." };
+        return { success: true, msg };
     };
 }
 
